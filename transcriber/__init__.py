6870d173923dfb20dd58aa4c5db353b22bb63685
babara6666 <tnfsh31219@gmail.com>
2026-01-19 15:52:35 +1300

    Fix: Update __init__.py imports and remove audio_converter conflicts

--- transcriber/__init__.py
+++ transcriber/__init__.py
@@ -4,21 +4,6 @@ from .config import Config, load_config
 from .scanner import scan_directory, get_pending_files
 from .transcribe import transcribe_audio, TranscriptResult
 from .formatter import format_markdown, save_markdown
-from .audio_converter import (
-    convert_to_m4a,
-    batch_convert_to_m4a,
-    is_supported_audio,
-    SUPPORTED_FORMATS,
-)
-from .translator import (
-    translate_to_chinese,
-    is_english_text,
-    add_translation,
-)
-from .pdf_generator import (
-    markdown_to_pdf,
-    batch_convert_to_pdf,
-)
 
 __all__ = [
     "Config",
@@ -29,13 +14,4 @@ __all__ = [
     "TranscriptResult",
     "format_markdown",
     "save_markdown",
-    "convert_to_m4a",
-    "batch_convert_to_m4a",
-    "is_supported_audio",
-    "SUPPORTED_FORMATS",
-    "translate_to_chinese",
-    "is_english_text",
-    "add_translation",
-    "markdown_to_pdf",
-    "batch_convert_to_pdf",
 ]
